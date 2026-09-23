f7d528d0fba99f5e0595f5aa2da44c9aa63bcd9b
Tejas Kumar Singh <tejaskumarsingh01@gmail.com>
2026-09-24 00:03:36 +0530

    Time: 4 ms (13.8%), Space: 9.9 MB (76.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int start = 0;
+        int end = n-1;
+
+        while(start < end){
+            while (start < end && !isalnum(s[start])){
+                start++;
+            }
+            while (start < end && !isalnum(s[end])){
+                end--;
+            }
+            if(tolower(s[start]) != tolower(s[end]))
+            return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
